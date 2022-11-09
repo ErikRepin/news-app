41d6a7de04cc780e85b8607a4c5ea1f3dd0b6952
rmehtije <rmehtije@gmail.com>
2022-11-09 16:54:35 +0200

    forms & layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import NewsGroupComponent from './Body/NewsGpoup';
-import HeaderComponet from './Body/Header';
+import NewsGroupComponent from './Body';
+import HeaderComponet from './Header';
+import PaginationComponent from './Footer';
 
 function App() {
   return (
       <Container>
         <HeaderComponet />
         <NewsGroupComponent />
+        <PaginationComponent />
       </Container>
   );
 }
