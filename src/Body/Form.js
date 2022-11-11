22e8e170585945b15eb5b3d4eaa8209f62d22588
rmehtije <rmehtije@gmail.com>
2022-11-11 16:59:20 +0200

    Form submit & pagination

--- src/Body/Form.js
+++ src/Body/Form.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function FormComponent({ show, handleClose }) {
+
+    const [startDateFrom, setStartDateFrom] = useState(new Date());
+    const [startDateTo, setStartDateTo] = useState(new Date());
+    const dateFormat = "dd-MM-yyyy";
 
     const languages = [
         { label: 'English', code: 'en' },
@@ -13,16 +21,33 @@ function FormComponent({ show, handleClose }) {
         { label: 'Spanish', code: 'es' },
     ];
 
+    function capitalize(str) {
+        return str[0].toUpperCase() + str.substring(1);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = {
+            q: event.target.q.value,
+            from: moment(event.target.from.value, dateFormat).format("YYYY-MM-DDT00:00:00.000"),
+            to: moment(event.target.to.value, dateFormat).format("YYYY-MM-DDT23:59:59.999"),
+            language: event.target.language.value,
+            searchIn: [...event.target.searchIn].filter(input => input.checked).map(input => input.value).join(','),
+        };
+        console.log(data);
+    }
+
     return (
         <Offcanvas show={show} onHide={handleClose}>
             <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Search News</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>Keywords</Form.Label>
-                        <Form.Control type="text" placeholder="Enter keywords or phrases" />
+                        <Form.Control type="text" name="q" placeholder="Enter keywords or phrases" />
                         <Form.Text className="text-muted">
                             Advanced search is supported here
                         </Form.Text>
@@ -31,9 +56,10 @@ function FormComponent({ show, handleClose }) {
                     {['title', 'description', 'content'].map((type) => (
                         <div key={`${type}`} className="mb-3">
                             <Form.Check
-                                label={type}
+                                label={capitalize(type)}
                                 name="searchIn"
                                 type="checkbox"
+                                value={type}
                                 id={`${type}-1`}
                             />
                         </div>
@@ -42,16 +68,29 @@ function FormComponent({ show, handleClose }) {
                     <Form.Group className="mb-3">
                         <Form.Label>From - to</Form.Label>
                         <InputGroup className="mb-3">
-                            <Form.Control aria-label="from" />
-                            <Form.Control aria-label="to" />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateFrom}
+                                onChange={(date) => setStartDateFrom(date)}
+                                name="from"
+                                dateFormat={dateFormat}
+                            />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateTo}
+                                onChange={(date) => setStartDateTo(date)}
+                                name="to"
+                                dateFormat={dateFormat}
+                            />
+
                         </InputGroup>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>Select Language</Form.Label>
-                        <Form.Select>
+                        <Form.Select name="language">
                             {languages.map((lang) => (
-                                <option>{lang.label}</option>
+                                <option key={lang.code} value={lang.code}>{lang.label}</option>
                             ))}
                         </Form.Select>
                     </Form.Group>
